b3ce8df10c959e8c6e6d06ffdbbbbdd51594079b
Megan Sinafaaganu Tuimauga <megant@missionreadyhq.com>
2022-05-19 00:06:13 +1200

    added data to js file

--- WEEK 02/javascriptintro.js
+++ WEEK 02/javascriptintro.js
@@ -1,1 +1,6 @@
-console.log("Hello World from the external javascript file");
+console.log("Hello World from the external javascript file"); //MY PRACTICE JAVASCRIPT CODE
+
+let firstName = 'Meggy'; // let is a variable that can be changed.. const is a variable that cannot be changed.
+console.log(firstName)
+firstName = 'Megan';
+console.log(firstName)
